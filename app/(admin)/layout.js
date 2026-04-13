bc67f05f945ad9c0906b4b8f73e9283a315a1628
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 18:52:33 +0800

    Update layout.js

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -1,15 +1,11 @@
-'use client';
-import React, { Suspense } from 'react';
-import RefineApp from './RefineApp';
-import { AntdRegistry } from "@ant-design/nextjs-registry";
-export default function AdminLayout({ children }) {
+import './globals.css'
+
+export default function RootLayout({ children }) {
   return (
-    <AntdRegistry>
-      <Suspense fallback={<div style={{ padding: '24px', background: '#0f172a', height: '100vh', color: '#fff' }}>Loading clowand OS...</div>}>
-        <RefineApp>
-          {children}
-        </RefineApp>
-      </Suspense>
-    </AntdRegistry>
-  );
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
 }
