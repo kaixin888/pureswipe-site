c521d64da783a5053e609bfa2771e74b0fd7252e
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 16:20:52 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
