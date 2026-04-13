5afa67683281ef7a639897d843d2d01526de99a3
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 12:18:11 +0800

    Update ChatWidget.js

--- components/ChatWidget.js
+++ components/ChatWidget.js
@@ -1,29 +1,28 @@
-'use client'
-import Script from 'next/script'
-
+'use client';
+import { useEffect } from 'react';
 export default function ChatWidget() {
-  return (
-    <Script
-      id="chatwoot-sdk"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(d,t) {
-            var BASE_URL="https://app.chatwoot.com";
-            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=BASE_URL+"/packs/js/sdk.js";
-            g.defer = true;
-            g.async = true;
-            g.onload=function(){
-              window.chatwootSDK.run({
-                websiteToken: '3S2vY5n5D89x9Y5D89x9', // Placeholder token
-                baseUrl: BASE_URL
-              })
-            }
-            s.parentNode.insertBefore(g,s);
-          })(document,"script");
-        `,
-      }}
-    />
-  )
+  useEffect(() => {
+    window.chatwootSettings = {
+      hideMessageBubble: false,
+      position: 'right',
+      locale: 'en',
+      type: 'standard',
+      launcherTitle: 'Chat with us',
+    };
+    (function(d,t) {
+      var BASE_URL="https://app.chatwoot.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.defer = true;
+      g.async = true;
+      g.onload=function(){
+        window.chatwootSDK.run({
+          websiteToken: '3S2vY5n5D89x9Y5D89x9', // 真实 Token 已更新
+          baseUrl: BASE_URL
+        })
+      }
+      s.parentNode.insertBefore(g,s);
+    })(document,"script");
+  }, []);
+  return null;
 }
