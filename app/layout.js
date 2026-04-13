7706706773b276dde464d2c73914aa200a08020b
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 09:38:30 +0800

    Remove font import and className from layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
