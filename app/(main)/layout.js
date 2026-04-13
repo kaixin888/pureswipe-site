37318201b050708a5408e1287063428b7fe23370
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 16:23:00 +0800

    Update layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../../globals.css'
-import Navbar from '../../../components/Navbar'
-import Footer from '../../../components/Footer'
-import ChatWidget from '../../../components/ChatWidget'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import ChatWidget from '../../components/ChatWidget'
 
 export default function SiteLayout({ children }) {
   return (
