ef7a93d8a714796d9c2d1e16aefc20d90e8c06ac
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 16:52:42 +0800

    Refactor AdminLayout to SiteLayout with new components

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,16 +1,16 @@
-'use client';
-import React, { Suspense } from 'react';
-import RefineApp from './RefineApp';
-import { AntdRegistry } from "@ant-design/nextjs-registry";
+import React from 'react'
+import '../../globals.css'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import ChatWidget from '../../components/ChatWidget'
 
-export default function AdminLayout({ children }) {
+export default function SiteLayout({ children }) {
   return (
-    <AntdRegistry>
-      <Suspense fallback={<div style={{ padding: '24px', background: '#0f172a', height: '100vh', color: '#fff' }}>Loading clowand OS...</div>}>
-        <RefineApp>
-          {children}
-        </RefineApp>
-      </Suspense>
-    </AntdRegistry>
-  );
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+      <ChatWidget />
+    </>
+  )
 }
