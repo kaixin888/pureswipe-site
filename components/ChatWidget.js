ad06c4c421c3c9c8ad73d753f04b1cccabc5e67a
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 09:39:24 +0800

    Create ChatWidget.js

---
+++ components/ChatWidget.js
@@ -0,0 +1,29 @@
+'use client'
+import Script from 'next/script'
+
+export default function ChatWidget() {
+  return (
+    <Script
+      id="chatwoot-sdk"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(d,t) {
+            var BASE_URL="https://app.chatwoot.com";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.defer = true;
+            g.async = true;
+            g.onload=function(){
+              window.chatwootSDK.run({
+                websiteToken: '3S2vY5n5D89x9Y5D89x9', // Placeholder token
+                baseUrl: BASE_URL
+              })
+            }
+            s.parentNode.insertBefore(g,s);
+          })(document,"script");
+        `,
+      }}
+    />
+  )
+}
