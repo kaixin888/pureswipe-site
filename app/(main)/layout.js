bf2eed38d500db0a93cf52e19b7160d705521554
kaixin888 <53389198+kaixin888@users.noreply.github.com>
2026-04-13 17:28:30 +0800

    Fix import path for globals.css in layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../../globals.css'
+import '../globals.css'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import ChatWidget from '../../components/ChatWidget'
