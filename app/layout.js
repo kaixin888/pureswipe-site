86dfa93769ea87b98d1af4299db167b025edd8e2
Chang <qufu345@gmail.com>
2026-04-17 22:15:46 +0800

    feat(#199): replace GTM with GA4 direct integration (G-JFTMBGD8EM)

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -40,14 +41,21 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="google" content="notranslate" />
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-PNBVHTGR");` }} />
       </head>
       <body className={inter.className}>
-        {/* GTM noscript fallback */}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNBVHTGR" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
-        </noscript>
+        {/* Google Analytics GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JFTMBGD8EM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JFTMBGD8EM');
+          `}
+        </Script>
         {children}
       </body>
     </html>
